3d42e276c2f23ba7bb1d288f5e6f99c5acf6f625
angelestarita <101217260+xAd4@users.noreply.github.com>
2024-07-30 19:23:31 -0400

    Add TemplateCoreViews in urls.py and add app core in settings

--- core/urls.py
+++ core/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
 from . import views
+
+
+urlpatterns = [
+    path('', views.HomeTemplateView.as_view(), name="home"),
+    path('portfolio/', views.PortfolioTemplateView.as_view(), name="portfolio"),
+    path('blog/', views.BlogTemplateView.as_view(), name="blog"),
+    path('contact/', views.ContactTemplateView.as_view(), name="contact"),
+]
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# Core Views Import
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # CORE VIEWS
+    path("", include("core.views")),
 ]
