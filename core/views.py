b0f29092feb8343c7daa7575f6d439f02e45269d
angelestarita <101217260+xAd4@users.noreply.github.com>
2024-07-30 19:19:28 -0400

    TemplatesPagesView with CBV

--- core/views.py
+++ core/views.py
@@ -2,3 +2,23 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
+
+# Home Page
+class HomeTemplateView(TemplateView):
+    template_name = "core/index.html"
+
+# Portfolio and Projects Page
+class PortfolioTemplateView(TemplateView):
+    template_name = "core/portfolio-details.html"
+
+# Blog Page
+class BlogTemplateView(TemplateView):
+    template_name = "core/blog-single.html"
+
+# Contact Page
+class ContactTemplateView(TemplateView):
+    template_name = "core/contact.html"
+
+# Error 404 Page
+class ErrorTemplateView(TemplateView):
+    template_name = "core/404.html"
