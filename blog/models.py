ea5ff8a4909f9a2560b0e9140188cb5fd634fc65
angelestarita <101217260+xAd4@users.noreply.github.com>
2024-08-02 21:38:59 -0400

    Method Security Models Post - Blog

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,15 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import User
+
+
+# Method Security 1
+def custom_upload_to(instance, filename):
+    if instance.pk:  # Verifica si la instancia ya tiene una pk
+        old_instance = Post.objects.get(pk=instance.pk)
+        old_instance.image.delete()
+    return 'post/' + filename
 
 # Create your models here.
 
@@ -8,7 +18,7 @@ class Post(models.Model):
     user_published = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     content = models.TextField()
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
     article = models.CharField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -19,3 +29,9 @@ class Post(models.Model):
         
     def __str__(self):
         return f"{self.user_published.username} - {self.title[20:]}"
+    
+# Method Security 1.2
+@receiver(post_delete, sender=Post)
+def delete_image_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        instance.image.delete(False)
