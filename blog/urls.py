d44054dc6242061f4b918b31486c3053cd410d89
angelestarita <101217260+xAd4@users.noreply.github.com>
2024-08-03 15:10:19 -0400

    POST CRUD Implemented

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # POST LIST
     path('', views.PostListView.as_view(), name="blog"),
+    # POST DETAIL
     path('<int:pk>/', views.PostDetailView.as_view(), name="blog-detail"),
+    # COMMENTS
     path("delete/<int:pk>/", views.CommentDeleteView.as_view(), name="blog-detail-delete"),
+    # POST CRUD
+    path("post/create/", views.PostCreateView.as_view(), name="blog-create"),
+    path("post/update/<int:pk>/", views.PostUpdateView.as_view(), name="blog-update"),
+    path("post/delete/<int:pk>/", views.PostDeleteView.as_view(), name="blog-delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.views.generic import TemplateView, ListView, DetailView, DeleteView
+from django.utils.decorators import method_decorator
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import TemplateView, ListView, DetailView, DeleteView, CreateView, UpdateView
 from .models import Post, Comment
 from .forms import CommentForm
 
 # Create your views here.
+
+
+# SECURITY METHOD
+"""class StaffIsRequired(object): 
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StaffIsRequired, self).dispatch(request, *args, **kwargs)"""
 
 # Blog Page
 class PostListView(ListView):
@@ -39,18 +48,45 @@ class PostDetailView(DetailView):
         context = self.get_context_data()
         context['form'] = form
         return self.render_to_response(context)
+    
+    
+# Blog CRUD - Only Staff's
+@method_decorator(staff_member_required, name="dispatch")
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "forms/default_form_create.html"
+    fields = ["title", "content", "image", "article"]
+    success_url = reverse_lazy("blog")
+    
+    def form_valid(self, form):
+        form.instance.user_published = self.request.user
+        return super().form_valid(form)
+    
+@method_decorator(staff_member_required, name="dispatch")
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "forms/default_form_update.html"
+    fields = ["title", "content", "image", "article"]
+    success_url = reverse_lazy("blog")
+    
+
+@method_decorator(staff_member_required, name="dispatch")
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "forms/default_form_delete.html"
+    success_url = reverse_lazy("blog")
+
 # Comments
-
-# Comment Detail
-class CommentDetailView(DetailView):
-    pass
-
 # Comment Delete
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = "forms/default_form_delete.html"
-    success_url = reverse_lazy("blog")
+    context_object_name = "object"
     
     def test_func(self):
         comment = self.get_object()
         return self.request.user == comment.user_published or self.request.user.is_superuser
+    
+    def get_success_url(self):
+        comment = self.get_object()
+        return reverse_lazy('blog-detail', kwargs={'pk': comment.post.id})
