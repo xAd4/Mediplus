aeb15496b50aa6cac731a9a91ac98bc67137ec53
angelestarita <101217260+xAd4@users.noreply.github.com>
2024-08-02 21:15:36 -0400

    Model Post created - Blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+    
+admin.site.register(Post, PostAdmin)
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Post
+class Post(models.Model):
+    user_published = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    article = models.CharField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        
+    def __str__(self):
+        return f"{self.user_published.username} - {self.title[20:]}"
